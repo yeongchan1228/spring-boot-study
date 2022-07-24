e75c24901dab0fb6f540ba5817fac235c7eb4dc6
최영찬 <gks54410@gmail.com>
2022-07-24 15:07:32 +0900

    Feat: #5 - Spring 적용

---
+++ src/main/java/study/spring/config/Config.java
@@ -0,0 +1,10 @@
+package study.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("study.spring")
+public class Config {
+
+}
