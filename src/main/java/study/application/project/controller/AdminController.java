0120ad597899980378cc54e841dfbd7bc936f161
최영찬 <gks54410@gmail.com>
2022-07-26 13:47:50 +0900

    Feat: #20 - 요청 데이터 출력

--- src/main/java/study/application/project/controller/AdminController.java
+++ src/main/java/study/application/project/controller/AdminController.java
@@ -4,20 +4,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import study.application.project.domain.constant.PlaceType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     @GetMapping("/places")
-    public String adminPlaces() { return "admin/places/places"; }
+    public ModelAndView adminPlaces(@RequestParam(required = false) PlaceType placeType,
+                                    @RequestParam(required = false) String placeName,
+                                    @RequestParam(required = false) String address) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeType);
+        map.put("placeName", placeName);
+        map.put("address", address);
+
+        return new ModelAndView("admin/places", map);
+    }
 
     @GetMapping("/places/{placeId}")
-    public String adminPlaceDetail(@PathVariable Integer placeId) { return "admin/place-detail"; }
+    public String adminPlaceDetail(@PathVariable Integer placeId) {
+        return "admin/place-detail";
+    }
 
     @GetMapping("/events")
-    public String adminEvents() { return "admin/events"; }
+    public String adminEvents() {
+        return "admin/events";
+    }
 
     @GetMapping("/events/{eventId}")
-    public String adminEventDetail(@PathVariable Integer eventId) { return "admin/event-detail"; }
+    public String adminEventDetail(@PathVariable Integer eventId) {
+        return "admin/event-detail";
+    }
 }
