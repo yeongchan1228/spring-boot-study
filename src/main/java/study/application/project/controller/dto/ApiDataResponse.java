c424579cdd509356f9652a3619c3278e3b635cc5
최영찬 <gks54410@gmail.com>
2022-07-26 14:03:35 +0900

    Feat: #20 - 응답 Dto 추가

---
+++ src/main/java/study/application/project/controller/dto/ApiDataResponse.java
@@ -0,0 +1,28 @@
+package study.application.project.controller.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import study.application.project.exception.ErrorCode;
+
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class ApiDataResponse<T> extends ApiErrorResponse {
+
+    private final T data;
+
+    private ApiDataResponse(T data) {
+        super(true, ErrorCode.OK.getCode(), ErrorCode.OK.getMessage());
+        this.data = data;
+    }
+
+    public static <T> ApiDataResponse<T> of(T data) {
+        return new ApiDataResponse<>(data);
+    }
+
+    public static <T> ApiDataResponse<T> empty() {
+        return new ApiDataResponse<>(null);
+    }
+
+}
