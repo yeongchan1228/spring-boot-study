0691fcad528d4a83b71c4e4bb65145134544e6ba
최영찬 <gks54410@gmail.com>
2022-07-27 19:23:27 +0900

    Feat: #20 - Controller, Service, Repository 연결

---
+++ src/main/java/study/application/project/service/EventService.java
@@ -0,0 +1,25 @@
+package study.application.project.service;
+
+import study.application.project.controller.dto.EventDto;
+import study.application.project.domain.constant.EventStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 이벤트 서비스
+ */
+public interface EventService {
+
+    /**
+     *
+     * @param placeId 장소 아이디
+     * @param eventName 이벤트 이름
+     * @param eventStatus 이벤트 상태
+     * @param eventStartDateTime 이벤트 시작 시간
+     * @param eventEndDateTime 이벤트 종료 시간
+     * @return 이벤트 목록
+     */
+    List<EventDto> getEvents(Long placeId, String eventName, EventStatus eventStatus,
+                              LocalDateTime eventStartDateTime, LocalDateTime eventEndDateTime);
+}
---
+++ src/main/java/study/application/project/service/EventServiceImpl.java
@@ -0,0 +1,45 @@
+package study.application.project.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import study.application.project.controller.dto.EventDto;
+import study.application.project.domain.constant.EventStatus;
+import study.application.project.repository.EventRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceImpl implements EventService {
+
+    private final EventRepository eventRepository;
+
+    @Override
+    public List<EventDto> getEvents(
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDateTime,
+            LocalDateTime eventEndDateTime
+    ) {
+        return eventRepository.findEvents(placeId, eventName, eventStatus, eventStartDateTime, eventEndDateTime);
+    }
+
+    public Optional<EventDto> getEvent(Long eventId) {
+        return eventRepository.findEvent(eventId);
+    }
+
+    public boolean createEvent(EventDto eventDto) {
+        return eventRepository.save(eventDto);
+    }
+
+    public boolean modifyEvent(Long eventId, EventDto eventDto) {
+        return eventRepository.update(eventId, eventDto);
+    }
+
+    public boolean deleteEvent(Long eventId) {
+        return eventRepository.delete(eventId);
+    }
+}
