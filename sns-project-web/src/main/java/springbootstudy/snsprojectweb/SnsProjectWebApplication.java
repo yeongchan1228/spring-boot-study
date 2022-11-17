d2c7a574a9338785a8b56e0e465f6a31bd4d9a5d
ycchoi <gks54410@gmail.com>
2022-11-17 21:20:30 +0900

    Initial Commit

---
+++ sns-project-web/src/main/java/springbootstudy/snsprojectweb/SnsProjectWebApplication.java
@@ -0,0 +1,13 @@
+package springbootstudy.snsprojectweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsProjectWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsProjectWebApplication.class, args);
+	}
+
+}
---
+++ sns-project-web/src/test/java/springbootstudy/snsprojectweb/SnsProjectWebApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootstudy.snsprojectweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsProjectWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
