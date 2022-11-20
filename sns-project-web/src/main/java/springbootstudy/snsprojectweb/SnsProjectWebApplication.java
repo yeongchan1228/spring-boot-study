4e86735d79ffc8901a8669a5f4f74175c7e539bb
ycchoi <gks54410@gmail.com>
2022-11-20 15:00:43 +0900

    fix: jpa auditing 활성화 분리

--- sns-project-web/src/main/java/springbootstudy/snsprojectweb/SnsProjectWebApplication.java
+++ sns-project-web/src/main/java/springbootstudy/snsprojectweb/SnsProjectWebApplication.java
@@ -2,14 +2,19 @@ package springbootstudy.snsprojectweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class SnsProjectWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnsProjectWebApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
---
+++ sns-project-web/src/main/java/springbootstudy/snsprojectweb/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package springbootstudy.snsprojectweb.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
